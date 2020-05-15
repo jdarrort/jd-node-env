597528eff36e881b6c04495438bcb2aaf757e893
jdarrort <jdarrort@gmail.com>
2020-05-15 14:03:20 +0200

    Init

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var path = require('path');
+
+const SECRET_PATH = '/run/secrets';
+try {
+    fs.readdirSync( SECRET_PATH )
+    .forEach( f => {
+        if (typeof process.env[f] === "undefined") { 
+            process.env[f] = fs.readFileSync(path.join(SECRET_PATH, f)); 
+        }
+    });
+} catch(e) {
+    console.warn("Failed to get secrets ");
+}
